9465df5f48323c133335dd50ee9a008369807916
Michael van der Weg <michael@vanderweg.ch>
2018-09-18 22:38:17 +0200

    chore: create first files :D

---
+++ src/mkdir.mjs
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+
+const { promises: { mkdir, stat } } = fs; 
+
+
+
+class DirectoryCreater {
+
+    create(directory) {
+        this.pathParts = directory.split(path.sep);
+    }
+
+
+
+
+
+}
+
+
+
+
+export default function(directory) {
+     
+}
+  
